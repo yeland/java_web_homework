124f51168ad2add42a484516db21f458080fa7c1
yeland <wlx100715@163.com>
2019-05-06 22:51:25 +0800

    Init project.

--- src/main/java/com/weilinxiao/homework/Dao/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Dao;
-
-import com.example.demo.tool.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-}
